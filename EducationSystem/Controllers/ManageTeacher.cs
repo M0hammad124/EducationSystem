4b79e279c4fec51e637c78a39ee0f3c345e55ceb
M0hammad124 <Nimoli124@gmail.com>
2021-11-30 09:54:18 +0330

    create ManageTeacher

---
+++ EducationSystem/Controllers/ManageTeacher.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducationSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManageTeacher : ControllerBase
+    {
+    }
+}
